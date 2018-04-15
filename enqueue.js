c99b8b05b714f30b884f40de9b90b5b4e8b02c3e
Roberto Falconi <falkons@outlook.com>
2018-04-15 19:05:32 +0200

    Fixed make an order button, login function, profile informations, minor style fixes

--- enqueue.js
+++ enqueue.js
@@ -5,13 +5,15 @@ var isEnqueued = false;
 function enqueue() {
     if (isEnqueued){
         if(confirm("Are you sure?")){
-            document.getElementById("you").innerHTML = "<input type=\"button\" value=\"Enqueue\" id=\"en_button\" onclick=\"enqueue()\"/>";
+            document.getElementById("you").innerHTML = "<input type=\"button\" class=\"button\" value=\"Enqueue\" id=\"en_button\" onclick=\"enqueue()\"/>";
             isEnqueued = false;
+            
         }
     } else {
         if(confirm("Are you sure?")){
-            document.getElementById("you").innerHTML = "You! <input type=\"button\" value=\"Delete\" id=\"en_button\" onclick=\"enqueue()\"/>";
+            document.getElementById("you").innerHTML = "You! &nbsp;&nbsp;&nbsp;&nbsp; <input type=\"button\" class=\"button\" value=\"Delete\" id=\"en_button\" onclick=\"enqueue()\"/> &nbsp;&nbsp;&nbsp;&nbsp; <input type=\"button\" class=\"button\" value=\"Make an order!\" id=\"en_button\" onclick=\"go_restaurant()()\"/>";
             isEnqueued = true;
+            
         }
     }
 }
