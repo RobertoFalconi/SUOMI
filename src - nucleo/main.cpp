6fcb122401d89c569951558821da0982cb616c60
Salvatore Licitra <salvolicitra95@gmail.com>
2018-05-31 23:23:04 +0200

    bugs correction

--- src - nucleo/main.cpp
+++ src - nucleo/main.cpp
@@ -8,7 +8,7 @@ InterruptIn button(USER_BUTTON);
 
 int result;
 int len = 20;
-int t,j,x;
+int values,count,trash;
 int myBPM;
 
 void pressed(){
@@ -20,18 +20,24 @@ void sendDataToProcessing(char symbol, int data){
         if(j<len){
             result = data<50?50:(data>120?120:data);
             //pc.printf("\rLoading");
-            j++;
+            count++;
             //for(int i=0;i<(j%4);++i) pc.printf(" .");
-            if(result<51 || result >119) x++;
-            else    t+=result;
+            if(result<51 || result >119) trash++;
+            else    values+=result;
     
         }
-        if(j==len){
-            led = 1;
-            myBPM=t/(len-x);
-            pc.printf("\rYour average BPM are: %d\r\n[Black]\trestart the measurement\r\n[Blue]\tconfirm the data\r\n",myBPM);
-            button.fall(&pressed);
-            j++;
+        if(count==len){
+            myBPM=values/(len-trash);
+            if(myBPM == 0){//if the result is composed only 
+                pc.printf("\rOps, something was wrong, I restart the measurement\r\n");
+                 led=0;count=0;values=0;trash=0;
+            }
+            else{
+                led = 1;
+                pc.printf("\rYour average BPM are: %d\r\n[Black]\trestart the measurement\r\n[Blue]\tconfirm the data\r\n",myBPM);
+                button.fall(&pressed);
+                count++;
+            }
         }
     }    
 }
@@ -40,7 +46,7 @@ int main() {
     pc.baud(115200);
     pc.printf("\r\n+++++++++++++++++++++++++++++++++++++++++++++++");    
     pc.printf("\r\nStart to inizialize the process\n");
-    led=0;j=0;t=0;x=0;
+    led=0;count=0;trash=0;values=0;
     PulseSensor sensor(A0, sendDataToProcessing);
     while(1) {
         sensor.start();
